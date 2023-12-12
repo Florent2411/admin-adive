cf332161936fbbfa4bf661b8b1ed0b1a7dc5f5e1
BADINI Rachid Rodrigue <bsrodrigue@gmail.com>
2023-12-12 18:55:05 +0000

    refactor(sidebar): extract sidebar links into array for better code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,93 +1,106 @@
 import React from 'react';
 
-function Sidebar() {
+const sidebarItems = [
+  {
+    title: "Tableau de Bord",
+    link: "/dashboard",
+    icon: "apps",
+  },
+  {
+    title: "Mes Emplois",
+    link: "/my-jobs",
+    icon: "book-alt",
+  },
+  {
+    title: "Emplois",
+    link: "/jobs",
+    icon: "book-alt",
+  },
+  {
+    title: "Mes Formations",
+    link: "/my-formations",
+    icon: "analysis",
+  },
+  {
+    title: "Formations",
+    link: "/formations",
+    icon: "analysis",
+  },
+  {
+    title: "Mes Certifications",
+    link: "/my-certifications",
+    icon: "award",
+  },
+  {
+    title: "Créer Mon CV",
+    link: "/create-cv",
+    icon: "plus-circle",
+  },
+  {
+    title: "Mes CVs",
+    link: "/my-cvs",
+    icon: "plus-circle",
+  },
+  {
+    title: "Mes Souscriptions",
+    link: "/my-subscriptions",
+    icon: "comments",
+  },
+  {
+    title: "Services",
+    link: "/services",
+    icon: "bell",
+  },
+
+]
+
+const sidebarBottomItems = [
+  {
+    title: "Paramètres",
+    link: "/settings",
+    icon: "cog"
+  },
+  {
+    title: "Laisser Un Message",
+    link: "/contact",
+    icon: "comment-alt-exclamation"
+  },
+]
+
+export default function Sidebar() {
 
   return (
     <nav className="vertical_nav">
       <div className="left_section menu_left" id="js-menu">
         <div className="left_section">
           <ul>
-            <li className="menu--item">
-              <a href="/dashboard" className="menu--link active" title='Tableau De Bord'>
-                <i className="uil uil-apps menu--icon" />
-                <span className="menu--label">Tableau De Bord</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Mes Emplois'>
-                <i className="uil uil-book-alt menu--icon" />
-                <span className="menu--label">Mes Emplois</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="/jobs" className="menu--link" title='Emplois'>
-                <i className="uil uil-book-alt menu--icon" />
-                <span className="menu--label">Emplois</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Mes Formations'>
-                <i className="uil uil-analysis menu--icon" />
-                <span className="menu--label">Mes Formations</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="/trainings" className="menu--link" title='Formations'>
-                <i className="uil uil-analysis menu--icon" />
-                <span className="menu--label">Formations</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Mes Certifications'>
-                <i className="uil uil-award menu--icon" />
-                <span className="menu--label">Mes Certifications</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="/create-cv" className="menu--link" title='Créer Mon CV'>
-                <i className="uil uil-plus-circle menu--icon" />
-                <span className="menu--label">Créer Mon CV</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="/my-cvs" className="menu--link" title='Mes CVs'>
-                <i className="uil uil-plus-circle menu--icon" />
-                <span className="menu--label">Mes CVs</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Mes Souscriptions'>
-                <i className="uil uil-comments menu--icon" />
-                <span className="menu--label">Mes Souscriptions</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Services'>
-                <i className="uil uil-bell menu--icon" />
-                <span className="menu--label">Services</span>
-              </a>
-            </li>
+            {
+              sidebarItems.map((item) => (
+                <li className="menu--item">
+                  <a href={item.link} className="menu--link" title={item.title}>
+                    <i className={`uil uil-${item.icon} menu--icon`} />
+                    <span className="menu--label">{item.title}</span>
+                  </a>
+                </li>
+              ))
+            }
           </ul>
         </div>
         <div className="left_section pt-2">
           <ul>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Paramètres'>
-                <i className="uil uil-cog menu--icon" />
-                <span className="menu--label">Paramètres</span>
-              </a>
-            </li>
-            <li className="menu--item">
-              <a href="#" className="menu--link" title='Laisser Un Message'>
-                <i className="uil uil-comment-alt-exclamation menu--icon" />
-                <span className="menu--label">Laisser Un Message</span>
-              </a>
-            </li>
+            {
+              sidebarBottomItems.map((item) => (
+                <li className="menu--item">
+                  <a href={item.link} className="menu--link" title={item.title}>
+                    <i className={`uil uil-${item.icon} menu--icon`} />
+                    <span className="menu--label">{item.title}</span>
+                  </a>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </div>
     </nav>
   );
 }
-
-export default Sidebar;
